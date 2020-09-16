3a1866d213ea4000f0aea2995b538f33a3a7e955
Allen C <dadarksideofdamoon@yahoo.com>
2020-09-16 19:08:46 -0400

    broke down homepage ito componets and styled

---
+++ src/pages/homepage/homepage.componet.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.style.scss';
+import Directory from '../../componets/directory/directory.componet';
+
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+
+    </div>
+
+);
+
+export default Homepage;
