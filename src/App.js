6bd1746a19b78ccf232259e18ec2c8219bb4651d
Allen C <dadarksideofdamoon@yahoo.com>
2020-09-28 18:34:33 -0400

    added header to home

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.componet';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './componets/header/header.componet';
 
 
 function App() {
   return (
     <div >
+      <Header />  
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route  path='/shop' component={ShopPage} />
